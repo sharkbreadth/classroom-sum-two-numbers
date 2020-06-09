f0532a3495051b79e3eadcecc31e0d39bcf6a26f
Manoj Ramachandran <manoj.ramach@gmail.com>
2020-06-09 18:49:42 -0400

    creating test cases
    added four test cases

---
+++ sumtwonumbers_test.py
@@ -0,0 +1,15 @@
+import sumtwonumbers;
+
+def test_case1():
+    assert addtwonumbers(2,5) == 7;
+     
+def test_case2():
+    assert addtwonumbers(2,6) == 8;    
+    
+def test_case3():
+    assert addtwonumbers(0,0) == 0;
+
+def test_case4():
+    assert addtwonumbers(-2,3) == 1;
+
+    
